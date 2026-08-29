6626073cc375213dae78015e9db3eb1b7732abe5
olgazuyakova <olga.zuyakova@gmail.com>
2026-08-29 15:43:11 -0700

    oz-update: configure tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+
+// import { useState } from "react";
+import ozLogo from "@svg/oz-logo.svg";
+
+export default function App() {
+	return (
+		<>
+			<div className="flex gap-2 items-center">
+			  <img src={ozLogo} alt="OZTech logo" />
+				<h3>OZTECH Consulting</h3>
+			</div>
+		</>
+	);
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+
+import { resolve } from 'node:path';
+import  * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [react(), cloudflare()],
+	plugins: [react(), cloudflare(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@css': resolve(__dirname, './src/assets/css'),
+			'@svg': resolve(__dirname, './src/assets/svg'),
+		}
+	},
 });
