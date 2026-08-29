6794412d5e99c981489dde8e8dd522cb67467f7b
olgazuyakova <olga.zuyakova@gmail.com>
2026-08-29 16:46:24 -0700

    main: configure tailwind

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,66 +1,15 @@
 // src/App.tsx
 
 // import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-// import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-// import honoLogo from "./assets/hono.svg";
-import "./App.css";
+import ozLogo from "@svg/oz-logo.svg";
 
-function App() {
-	// const [count, setCount] = useState(0);
-	// const [name, setName] = useState("unknown");
-
+export default function App() {
 	return (
 		<>
-			{/* <div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-				<a href="https://hono.dev/" target="_blank">
-					<img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-				</a>
-				<a href="https://workers.cloudflare.com/" target="_blank">
-					<img
-						src={cloudflareLogo}
-						className="logo cloudflare"
-						alt="Cloudflare logo"
-					/>
-				</a>
-			</div> */}
-			<h3>OZTech Demo</h3>
-			{/* <div className="card">
-				<button
-					onClick={() => setCount((count) => count + 1)}
-					aria-label="increment"
-				>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div> */}
-			{/* <div className="card">
-				<button
-					onClick={() => {
-						fetch("/api/")
-							.then((res) => res.json() as Promise<{ name: string }>)
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>worker/index.ts</code> to change the name
-				</p>
+			<div className="flex gap-2 items-center">
+			  <img src={ozLogo} alt="OZTech logo" />
+				<h3>OZTECH Consulting</h3>
 			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p> */}
 		</>
 	);
 }
-
-export default App;
